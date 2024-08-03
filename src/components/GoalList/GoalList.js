07466dc16e1103a799c7ac39491016a651d23724
Karan <karan.l.k123321@gmail.com>
2024-08-03 17:45:45 +0530

    Render the props to another component

--- src/components/GoalList/GoalList.js
+++ src/components/GoalList/GoalList.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./GoalList.css";
 
-const GoalList = props => {
-  console.log(props.goals);
+const GoalList = (props) => {
+  //console.log(props.goals);
   return (
     <div className="goallist">
       This is from GoalList Component
-      {/* <ul className="list">
-        <li>SONY</li>
+      <ul className="list">
+        {props.goals.map((goals) => {
+          return <li>{goals.id} {goals.text}</li>;
+        })}
+
+        {/* <li>SONY</li>
         <li>JBL</li>
         <li>APPLE</li>
         <li>BOAT</li>
-        <li>REALME</li>
-      </ul> */}
+        <li>REALME</li>*/}
+      </ul>
     </div>
   );
 };
